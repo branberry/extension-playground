2bc443fcdcdbbd6ff98c10ed3e71aecd47cba745
wowbob396 <wowbob396@gmail.com>
2018-10-15 21:50:53 -0500

    started working on word counter

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,46 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "extension-playground" is now active!');
+    console.log('Congratulations, your extension "extension-playground: Word Counter" is now active!');
+    
+
+    // create a new word counter
+    let wordCounter = new WordCounter();
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        window.showInformationMessage('Hello World!');
     });
 
     context.subscriptions.push(disposable);
+}
+
+class WordCounter {
+    private _statusBarItem: StatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+
+    public updateWordCount() {
+
+        // get the current text editor
+        let editor = window.activeTextEditor;
+
+        if (!editor) {
+            this._statusBarItem.hide();
+            return;
+        }
+    }
 }
 
 // this method is called when your extension is deactivated
